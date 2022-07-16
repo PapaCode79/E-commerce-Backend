44762c3fa271e7a37e9079963773ff738fbce1ee
Badara Dia <pushsoccer79@gmail.com>
2022-07-16 13:36:11 -0400

    final commit

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,7 +14,7 @@ router.get('/',async (req, res) => {
   }
 });
 
-router.get('/:id',async (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
@@ -30,7 +30,7 @@ router.get('/:id',async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   try {
     const data = await Category.create(req.body)
